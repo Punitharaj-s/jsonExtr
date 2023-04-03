ebe1304d703097bdeeaf529d2c7aec5d3987fd16
Punitharaj S <83452431+Punitharaj-s@users.noreply.github.com>
2023-04-03 17:26:44 +0530

    Add files via upload

---
+++ jsonExtr.js
@@ -0,0 +1,5 @@
+//loading the json data 
+const config = require("./json/example_1.json");
+console.log(config);
+//fetch("../json/example_1.json").then((response)=>response.json()).then((data)=>console.log(data));
+            
